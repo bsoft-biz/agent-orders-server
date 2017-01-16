0864fb4a8eb38380aac64d3802662c357cfc12ff
Slava Babin <vbabin@bsoft.biz>
2017-01-16 15:36:48 +0200

    To use CSRF protection we have to provide csrf token to android client.

--- src/main/java/biz/bsoft/orders/spring/SecurityConfig.java
+++ src/main/java/biz/bsoft/orders/spring/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 /**
  * Created by vbabin on 30.08.2016.
@@ -38,6 +39,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                //.csrf().disable()
+                .csrf()
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                    .and()
                 .authorizeRequests()
                     .anyRequest().authenticated()
                     .and()
